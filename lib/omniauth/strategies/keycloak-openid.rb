fc4a21aa1fe6924c83331932e69650cd22d7de71
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-06-24 09:05:02 +0200

    Fix safe offenses in production code
    - Performance/RegexpMatch
    - Style/AccessorGrouping
    - Style/GuardClause
    - Style/HashSyntax
    - Style/ParenthesesAroundCondition
    - Style/RedundantAssignment
    - Style/RedundantParentheses
    - Style/RegexpLiteral
    - Style/StringLiterals

--- lib/omniauth/strategies/keycloak-openid.rb
+++ lib/omniauth/strategies/keycloak-openid.rb
@@ -9,57 +9,54 @@ module OmniAuth
       class ConfigurationError < Error; end
       class IntegrationError < Error; end
 
-      attr_reader :authorize_url
-      attr_reader :token_url
-      attr_reader :certs
+      attr_reader :authorize_url, :token_url, :certs
 
       def setup_phase
         super
 
-        if (@authorize_url.nil? || @token_url.nil?) && !OmniAuth.config.test_mode
+        return unless (@authorize_url.nil? || @token_url.nil?) && !OmniAuth.config.test_mode
 
-          prevent_site_option_mistake
+        prevent_site_option_mistake
 
-          realm = options.client_options[:realm].nil? ? options.client_id : options.client_options[:realm]
-          site = options.client_options[:site]
+        realm = options.client_options[:realm].nil? ? options.client_id : options.client_options[:realm]
+        site = options.client_options[:site]
 
-          raise_on_failure = options.client_options.fetch(:raise_on_failure, false)
+        raise_on_failure = options.client_options.fetch(:raise_on_failure, false)
 
-          config_url = URI.join(site, "#{auth_url_base}/realms/#{realm}/.well-known/openid-configuration")
+        config_url = URI.join(site, "#{auth_url_base}/realms/#{realm}/.well-known/openid-configuration")
 
-          log :debug, "Going to get Keycloak configuration. URL: #{config_url}"
-          response = Faraday.get config_url
-          if (response.status == 200)
-            json = JSON.parse(response.body)
+        log :debug, "Going to get Keycloak configuration. URL: #{config_url}"
+        response = Faraday.get config_url
+        if response.status == 200
+          json = JSON.parse(response.body)
 
-            @certs_endpoint = json["jwks_uri"]
-            @userinfo_endpoint = json["userinfo_endpoint"]
-            @authorize_url = URI(json["authorization_endpoint"]).path
-            @token_url = URI(json["token_endpoint"]).path
+          @certs_endpoint = json['jwks_uri']
+          @userinfo_endpoint = json['userinfo_endpoint']
+          @authorize_url = URI(json['authorization_endpoint']).path
+          @token_url = URI(json['token_endpoint']).path
 
-            log_config(json)
+          log_config(json)
 
-            options.client_options.merge!({
-                                            authorize_url: @authorize_url,
-                                            token_url: @token_url
-                                          })
-            log :debug, "Going to get certificates. URL: #{@certs_endpoint}"
-            certs = Faraday.get @certs_endpoint
-            if (certs.status == 200)
-              json = JSON.parse(certs.body)
-              @certs = json["keys"]
-              log :debug, "Successfully got certificate. Certificate length: #{@certs.length}"
-            else
-              message = "Couldn't get certificate. URL: #{@certs_endpoint}"
-              log :error, message
-              raise IntegrationError, message if raise_on_failure
-            end
+          options.client_options.merge!({
+                                          authorize_url: @authorize_url,
+                                          token_url: @token_url
+                                        })
+          log :debug, "Going to get certificates. URL: #{@certs_endpoint}"
+          certs = Faraday.get @certs_endpoint
+          if certs.status == 200
+            json = JSON.parse(certs.body)
+            @certs = json['keys']
+            log :debug, "Successfully got certificate. Certificate length: #{@certs.length}"
           else
-            message = "Keycloak configuration request failed with status: #{response.status}. " \
-                      "URL: #{config_url}"
+            message = "Couldn't get certificate. URL: #{@certs_endpoint}"
             log :error, message
             raise IntegrationError, message if raise_on_failure
           end
+        else
+          message = "Keycloak configuration request failed with status: #{response.status}. " \
+                    "URL: #{config_url}"
+          log :error, message
+          raise IntegrationError, message if raise_on_failure
         end
       end
 
@@ -67,21 +64,21 @@ module OmniAuth
         return '/auth' unless options.client_options[:base_url]
 
         base_url = options.client_options[:base_url]
-        return base_url if (base_url == '' || base_url[0] == '/')
+        return base_url if base_url == '' || base_url[0] == '/'
 
         raise ConfigurationError, "Keycloak base_url option should start with '/'. Current value: #{base_url}"
       end
 
       def prevent_site_option_mistake
         site = options.client_options[:site]
-        return unless site =~ /\/auth$/
+        return unless %r{/auth$}.match?(site)
 
         raise ConfigurationError, "Keycloak site parameter should not include /auth part, only domain. Current value: #{site}"
       end
 
       def log_config(config_json)
         log_keycloak_config = options.client_options.fetch(:log_keycloak_config, false)
-        log :debug, "Successfully got Keycloak config"
+        log :debug, 'Successfully got Keycloak config'
         log :debug, "Keycloak config: #{config_json}" if log_keycloak_config
         log :debug, "Certs endpoint: #{@certs_endpoint}"
         log :debug, "Userinfo endpoint: #{@userinfo_endpoint}"
@@ -90,10 +87,10 @@ module OmniAuth
       end
 
       def build_access_token
-        verifier = request.params["code"]
+        verifier = request.params['code']
         client.auth_code.get_token(verifier,
-                                   { :redirect_uri => callback_url.gsub(/\?.+\Z/, "") }
-                                   .merge(token_params.to_hash(:symbolize_keys => true)),
+                                   { redirect_uri: callback_url.gsub(/\?.+\Z/, '') }
+                                   .merge(token_params.to_hash(symbolize_keys: true)),
                                    deep_symbolize(options.auth_token_params))
       end
 
@@ -108,10 +105,10 @@ module OmniAuth
 
       info do
         {
-          :name => raw_info['name'],
-          :email => raw_info['email'],
-          :first_name => raw_info['given_name'],
-          :last_name => raw_info['family_name']
+          name: raw_info['name'],
+          email: raw_info['email'],
+          first_name: raw_info['given_name'],
+          last_name: raw_info['family_name']
         }
       end
 
@@ -125,8 +122,7 @@ module OmniAuth
       def raw_info
         id_token_string = access_token.token
         jwks = JSON::JWK::Set.new(@certs)
-        id_token = JSON::JWT.decode id_token_string, jwks
-        id_token
+        JSON::JWT.decode id_token_string, jwks
       end
 
       OmniAuth.config.add_camelization('keycloak_openid', 'KeycloakOpenId')
